a57ac81d64b391a0203d617fb75a5a04622436d8
LMatch Dev <dev@lmatch.ma>
2026-06-25 12:09:28 +0100

    design: replace brain circuit branding with highly optimized lightning bolt SVG design

--- src/components/LconqLogo.jsx
+++ src/components/LconqLogo.jsx
@@ -1,5 +1,3 @@
-import { BrainCircuit } from 'lucide-react';
-
 /**
  * LconqLogo component
  * Renders the original gradient box and BrainCircuit icon style of L'CONQ.
@@ -44,7 +42,9 @@ export default function LconqLogo({
           flexShrink: 0
         }}
       >
-        <BrainCircuit size={iconSize} color="#fff" />
+        <svg viewBox="0 0 48 46" style={{ width: iconSize, height: iconSize }} fill="#fff">
+          <path d="M25.946 44.938c-.664.845-2.021.375-2.021-.698V33.937a2.26 2.26 0 0 0-2.262-2.262H10.287c-.92 0-1.456-1.04-.92-1.788l7.48-10.471c1.07-1.497 0-3.578-1.842-3.578H1.237c-.92 0-1.456-1.04-.92-1.788L10.013.474c.214-.297.556-.474.92-.474h28.894c.92 0 1.456 1.04.92 1.788l-7.48 10.471c-1.07 1.498 0 3.579 1.842 3.579h11.377c.943 0 1.473 1.088.89 1.83L25.947 44.94z" />
+        </svg>
       </div>
 
       {showText && !iconOnly && (
